eb8314bae3c55f23c7d2897eca41b7e8b133d448
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-03 16:53:55 +0300

    refactor

--- src/component/howTo/HowToBreadcrumb.jsx
+++ src/component/howTo/HowToBreadcrumb.jsx
@@ -2,9 +2,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// Redux
-// import { connect } from "react-redux";
-
 // FontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
