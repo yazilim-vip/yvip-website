5d3b57d3884ad3fc2b1fd9646ab82a3088d64a76
maemresen <maemresen@users.noreply.github.com>
2020-11-25 20:50:40 +0000

    deploy: 62bc286376335334ecb32e5fccb880363903ff37

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fad0135a3a5a34eea6c1bfa08e674aed.js"
+  "/precache-manifest.6f630df87b5fc07931d5587adcea57dc.js"
 );
 
 self.addEventListener('message', (event) => {
