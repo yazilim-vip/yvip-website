d3381748534ee58d9fbd1b19822f4213d509e063
Emre Sen <maemresen@yazilim.vip>
2021-01-02 07:58:51 +0300

    I: top menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react'
+import { TopBar } from './component/TopBar'
 
 const App = () => {
-  return <div>main</div>;
-};
+    return (
+        <div>
+            {/* <!-- ======= Top Bar ======= --> */}
+            <TopBar mail='info@yazilim.vip' linkedinUrl='#' />
+        </div>
+    )
+}
 
-export default App;
+export default App
