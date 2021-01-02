dd07b6846e69779b5353697e38025f19eb97babd
Emre Sen <maemresen@yazilim.vip>
2021-01-02 18:33:20 +0300

    I: projcts moved to constant

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -29,111 +29,71 @@
       "assets/img/slide/birds.jpg"
     )
   );
+
+  const PROJECTS = [
+    {
+      title: "Play2Gether",
+      description: `
+      The Play2Gether app is a music listening platform to listen to Spotify 
+      with your friends or other people. Listen to the same song at the same time 
+      with other people, queue and vote the songs, chat together, discover and share new music!
+      `,
+      imageSource: "assets/img/portfolio/portfolio-1.jpg",
+    },
+    {
+      title: "TimeCountdown",
+      description: `
+        A visual studio code extension and this extension 
+        takes care of the time countdown tasks. It is very easy to use,
+        just type the time to count down in an allowed format, and 
+        the application will count down the given time by 1 sec in an interval.
+      `,
+      imageSource: "assets/img/portfolio/portfolio-2.jpg",
+      links: [
+        {
+          url:
+            "https://marketplace.visualstudio.com/items?itemName=yazilim-vip.time-countdown",
+          title: "Home Page",
+          icon: "fas fa-plug",
+        },
+      ],
+    },
+    {
+      title: "Spring VIP",
+      description: `
+        A Java Spring Library that provides helpful generic and utility classes. 
+        Some of features provided by library are generic CRUD Service and Rest controllers. 
+        See more from documentation
+      `,
+      imageSource: "assets/img/portfolio/portfolio-3.jpg",
+      links: [
+        {
+          url: "https://springvip.yazilim.vip",
+          title: "Documentation",
+          icon: "fas fa-book",
+        },
+      ],
+    },
+    {
+      title: "maescript",
+      description:
+        "'Contains useful functions with autocomplete feauteres and comes with useful set of shell scripts",
+      imageSource: "assets/img/portfolio/portfolio-3.jpg",
+      links: [
+        {
+          url: "https://yazilim-vip.github.io/maescript",
+          title: "Documentation",
+          icon: "fas fa-book",
+        },
+      ],
+    },
+  ];
+  
   $("#app").append(`
           <main id='main'>
-            ${createPortfolioSection([
-              {
-                title: "Play2Gether",
-                description: "Music Listening Platform",
-                imageSource: "assets/img/portfolio/portfolio-1.jpg",
-              },
-              {
-                title: "TimeCountdown",
-                description: "VS Code Extension",
-                imageSource: "assets/img/portfolio/portfolio-2.jpg",
-                links: [
-                  {
-                    url:
-                      "https://marketplace.visualstudio.com/items?itemName=yazilim-vip.time-countdown",
-                    title: "Home Page",
-                    icon: "fas fa-plug",
-                  },
-                ],
-              },
-              {
-                title: "Spring VIP",
-                description: "A Java Spring Library",
-                imageSource: "assets/img/portfolio/portfolio-3.jpg",
-                links: [
-                  {
-                    url: "https://springvip.yazilim.vip",
-                    title: "Documentation",
-                    icon: "fas fa-book",
-                  },
-                ],
-              },
-              {
-                title: "maescript",
-                description: "Linux Shell Helpers & Utilities",
-                imageSource: "assets/img/portfolio/portfolio-3.jpg",
-                links: [
-                  {
-                    url: "https://yazilim-vip.github.io/maescript",
-                    title: "Documentation",
-                    icon: "fas fa-book",
-                  },
-                ],
-              },
-            ])}
+            ${createPortfolioSection(PROJECTS)}
 
-            ${createSerivcesSection([
-              {
-                title: "Play2Gether",
-                description: `
-                The Play2Gether app is a music listening platform to listen to Spotify 
-                with your friends or other people. Listen to the same song at the same time 
-                with other people, queue and vote the songs, chat together, discover and share new music!
-                `,
-                imageSource: "assets/img/portfolio/portfolio-1.jpg",
-              },
-              {
-                title: "TimeCountdown",
-                description: `
-                  A visual studio code extension and this extension 
-                  takes care of the time countdown tasks. It is very easy to use,
-                  just type the time to count down in an allowed format, and 
-                  the application will count down the given time by 1 sec in an interval.
-                `,
-                imageSource: "assets/img/portfolio/portfolio-2.jpg",
-                links: [
-                  {
-                    url:
-                      "https://marketplace.visualstudio.com/items?itemName=yazilim-vip.time-countdown",
-                    title: "Home Page",
-                    icon: "fas fa-plug",
-                  },
-                ],
-              },
-              {
-                title: "Spring VIP",
-                description: `
-                  A Java Spring Library that provides helpful generic and utility classes. 
-                  Some of features provided by library are generic CRUD Service and Rest controllers. 
-                  See more from documentation
-                `,
-                imageSource: "assets/img/portfolio/portfolio-3.jpg",
-                links: [
-                  {
-                    url: "https://springvip.yazilim.vip",
-                    title: "Documentation",
-                    icon: "fas fa-book",
-                  },
-                ],
-              },
-              {
-                title: "maescript",
-                description:
-                  "'Contains useful functions with autocomplete feauteres and comes with useful set of shell scripts",
-                imageSource: "assets/img/portfolio/portfolio-3.jpg",
-                links: [
-                  {
-                    url: "https://yazilim-vip.github.io/maescript",
-                    title: "Documentation",
-                    icon: "fas fa-book",
-                  },
-                ],
-              },
-            ])}
+            ${createSerivcesSection(PROJECTS)}
             
             ${createTeamSection([
               {
