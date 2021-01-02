c9273e449feaa8909502e213c6cf6a30308042af
maemresen <maemresen@users.noreply.github.com>
2021-01-02 15:47:47 +0000

    deploy: 84f8641955b2410f500a70882dfbbfe425c44acc

--- assets/js/ui/createServicesSection.js
+++ assets/js/ui/createServicesSection.js
@@ -3,12 +3,41 @@ const createSerivcesSectionItem = ({
   description,
   imageSource,
   links,
+  labels
 }) => {
   return `     
         <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
             <div class="icon"><i class="icofont-computer"></i></div>
             <h4 class="title">${title}</h4>
+            <h4>
+              ${
+                links === undefined
+                  ? ""
+                  : links
+                      .map((link) => {
+                        return `
+                      <a href='${link.url}' title='${link.title}' target='_blank'>
+                          <i class='${link.icon}'></i>
+                      </a>
+                  `;
+                      })
+                      .join("")
+              }
+            </h4>
             <p class="description">${description}</p>
+
+            ${
+              labels === undefined
+                ? ""
+                : labels
+                    .map((label) => {
+                      return `
+                <span class="badge badge-pill badge-dark">${label}</span>
+                `;
+                    })
+                    .join("")
+            }
+            
         </div>
       `;
 };
@@ -20,14 +49,15 @@ const createSerivcesSection = (items) => {
       <div class="container">
 
         <div class="section-title">
-          <h2>Services</h2>
+          <h2>Our Portfolio</h2>
         </div>
 
         <div class="row">
-            ${items.map((each) => {
-                console.log(each)
-                return createSerivcesSectionItem(each)
-            }).join('')}
+            ${items
+              .map((each) => {
+                return createSerivcesSectionItem(each);
+              })
+              .join("")}
         </div>
 
       </div>
