5fc5243ed9353ce19a6dac285b66ad79b1313b7e
Emre Sen <maemresen@yazilim.vip>
2021-01-02 08:11:17 +0300

    I: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
 import React from 'react'
+
+import { Header } from './component/Header'
 import { TopBar } from './component/TopBar'
+import { SectionItem } from './component/types'
 
 const App = () => {
+    const sectionPortfolio = { id: 'portfolio', name: 'Portfolio' }
+    const sectionTeam = { id: 'team', name: 'Team' }
+    const sectionList: Array<SectionItem> = [sectionPortfolio, sectionTeam]
+
     return (
         <div>
             {/* <!-- ======= Top Bar ======= --> */}
             <TopBar mail='info@yazilim.vip' linkedinUrl='#' />
+            <Header logo='Yazilim VIP' sectionList={sectionList} />
+
+            <section id='hero'>
+                <div className='hero-container'>
+                    <div
+                        id='heroCarousel'
+                        className='carousel slide carousel-fade'
+                        data-ride='carousel'
+                    >
+                        <ol
+                            className='carousel-indicators'
+                            id='hero-carousel-indicators'
+                        ></ol>
+
+                        <div className='carousel-inner' role='listbox'>
+                            <div
+                                className='carousel-item active'
+                                style={{
+                                    backgroundImage:
+                                        "url('assets/img/slide/slide-1.jpg')"
+                                }}
+                            >
+                                <div className='carousel-container'>
+                                    <div className='carousel-content container'>
+                                        <h2 className='animate__animated animate__fadeInDown'>
+                                            Welcome to <span>Yazılım VIP</span>
+                                        </h2>
+                                        <p className='animate__animated animate__fadeInUp'>
+                                            To keep this platform open to
+                                            everyone who is competent and
+                                            willing to share what we call an
+                                            open source world. A platform aimed
+                                            at presenting our competencies to
+                                            the open source world without
+                                            waiting for a response.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
---
+++ src/component/Header.tsx
@@ -0,0 +1,31 @@
+import { SectionItem } from './types'
+
+export type HeaderType = {
+    logo: string
+    sectionList: Array<SectionItem>
+}
+export const Header = ({ logo, sectionList }: HeaderType) => (
+    <header id='header'>
+        <div className='container'>
+            <div className='logo float-left'>
+                <h1 className='text-light'>
+                    <a href='/s'>
+                        <span>{logo}</span>
+                    </a>
+                </h1>
+            </div>
+            <nav className='nav-menu float-right d-none d-lg-block'>
+                <ul>
+                    <li className='active'>
+                        <a href='/'>Home</a>
+                    </li>
+                    {sectionList.map((section) => (
+                        <li>
+                            <a href={`#${section.id}`}>{section.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    </header>
+)
